69d574a86baf918e27ae014ec5f034221ff64c7b
gordei513 <71318696+gordei513@users.noreply.github.com>
2020-10-25 14:18:30 +0300

    Create rabin_karp.py

---
+++ rabin_karp.py
@@ -0,0 +1,30 @@
+def rabin_karp(s, sub):
+    N = 0
+    K = 0
+    h_sub = sum(ord(c) for c in sub)
+    h = sum(ord(c) for c in s[:len(sub)])
+    for pos in range(0,  len(O)- len(sub)  ):
+    
+        if h != h_sub:
+            N += 1
+            h = h - ord(O[pos]) + ord(O[pos + len(sub)]) #,,сдвиг и проверка,,
+        h = h - ord(O[pos]) + ord(O[pos + len(sub)]) 
+        flag = True
+        for i in range(len(sub)):
+            if s[pos+i] != sub[i]:
+                flag = False
+                break
+            
+            K += 1 
+        if flag:
+    
+            print('Нашлось совпадение с этого номера =%d'%(pos+1))
+    print('Потребовалось N=%d проверок'%(N))
+    print('Всего сопадений: =%d' %(K/len(t)))
+
+
+
+s = 'ABCDABCDABCD' #Сама строка, в которой ищем подсктроку.
+t = 'ABCD' #Подстрока, которую ищем в строке
+O = s + '+' #Когда была длина s, то когда у подстроки не было справо других елементо, ее не считывало
+rabin_karp(s, t)
