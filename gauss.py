5ed2aed8bff7b0f0aff699620369c7f1bae9db3d
gordei513 <71318696+gordei513@users.noreply.github.com>
2022-04-10 13:45:59 +0300

    Update gauss.py

--- gauss.py
+++ gauss.py
@@ -5,23 +5,24 @@ from numpy.linalg import solve as solve_out_of_the_box
 a = array([
     [1, 2.0, 1.5, 2.0],
     [3.0, 2.0, 4.0, 1.0],
-    [1.0, 6.0, 0.0, 4  ],
-    [2.0, 1.0, 4.0, 3  ]
+    [1.0, 6.0, 0.0, 4],
+    [2.0, 1.0, 4.0, 3]
 ], dtype=float)
 b = array([5, 6, 7, 8], dtype=float)
+
 def vgauss(a, b):
-    ab = concatenate((a, array([b]).T), axis=1) 
-    dim = len(ab) 
-    ab[0]= ab[0]/ ab[0,0]
-    for k in range(1,dim):
-        for i  in range(k,dim):
-                ab[i] =  ((ab[i] - ab[k-1] * ab[i,k-1] / ab[k-1,k-1]))
-                ab[i] = ab[i]/(ab[i,i]);
+    ab = concatenate((a, array([b]).T), axis=1)
+    dim = len(ab)
+    ab[0] = ab[0]/ab[0, 0]
+    for k in range(1, dim):
+        for i in range(k, dim):
+            ab[i] = (ab[i] - ab[k-1] * ab[i, k-1])/ab[k-1, k-1]
+            ab[i] = ab[i]/(ab[i, i])
     for i in range(dim - 1, -1, -1):
-        for k  in range(i-1, -1, -1):
-            ab[k]=ab[k]-ab[i]*ab[k,i]
-    x = ab[:, -1] 
-    return(x)
+        for k in range(i-1, -1, -1):
+            ab[k] = ab[k]-ab[i]*ab[k, i]
+    x = ab[:, -1]
+    return x
 vgauss(a, b)
 oob_solution = solve_out_of_the_box(a, b)
 solution = vgauss(a, b)
